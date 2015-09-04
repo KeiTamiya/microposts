36dc1aba26b3e5ac0c6cb3e02bcdb2429f7ea671
KeiTamiya <webcreatorhouse@gmail.com>
2015-09-04 11:14:32 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ActiveRecord::Base
     validates :profile, presence: false, length: { maximum: 144 }
     validates :area, presence: false, length: { maximum: 50 }
     has_many :microposts
+    has_many :following_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :following_users, through: :following_relationships, source: :followed
+    has_many :followed_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :followed_users, through: :follower_relationships, source: :follower
+    
+    # follow other users
+    def follow(other_user)
+        following_relationships.create(followed_id: other_user.id)
+    end
+    
+    # unfollow other following users
+    def unfollow(other_user)
+        following_relationships.find_by(followed_id: other_user.id).destroy
+    end
+    
+    # whether follow such user or not
+    def following?(other_user)
+        following_users.include?(other_user)
+    end
 end
